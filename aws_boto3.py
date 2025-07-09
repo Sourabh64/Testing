fb1e20a6e05d3fde9f5232a73ef74f4ce43716f6
Sourabh S Kulkarni <37709891+Sourabh64@users.noreply.github.com>
2025-07-09 12:02:52 +0530

    Update aws_boto3.py

--- aws_boto3.py
+++ aws_boto3.py
@@ -1,8 +1,8 @@
 import boto3
 
 region_name = 'ap-south-1'
-role_arn = 'arn:aws:iam::117458339469:role/Tmura'
-session_name = '117458339469'
+role_arn = 'arn'
+session_name = 'session_details'
 client = boto3.client('sts',region_name=region_name)
 cred = client.assume_role(RoleArn=role_arn,RoleSessionName=session_name)
 cred = cred['Credentials']
